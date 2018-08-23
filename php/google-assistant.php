b96c8500e1487d729ec26a5302a9c84aa2357e2f
JPTN <nahpymerej@gmail.com>
2018-08-23 10:47:23 -0400

    Create google-assistant.php

---
+++ php/google-assistant.php
@@ -0,0 +1,43 @@
+<?
+$content = file_get_contents('php://input');
+$jsoncontent = json_decode($content);
+
+$intent = $jsoncontent->queryResult->intent->displayName;
+
+require 'bmw-api.php'; // gets data from BMW's API
+
+$JSONdataOUT = GetJsonMessageResponse($intent, $JSONdata);
+$size = strlen($JSONdataOUT);
+header('Content-Type: application/json');
+header("Content-length: $size");
+echo $JSONdataOUT;
+
+function GetJsonMessageResponse($intent,$JSONdata) {
+	global $SpeakPhrase;
+	
+	$ReturnValue = "";
+	include('text.php');
+	$DisplayValue = str_replace("eye three", "i3", $SpeakPhrase);
+
+	$ReturnValue= '
+	{
+	  "payload": {
+	    "google": {
+	      "expectUserResponse": false,
+	      "richResponse": {
+	        "items": [
+	          {
+	            "simpleResponse": {
+				"displayText": "'.$DisplayValue.'",
+				"textToSpeech": "'.$SpeakPhrase.'"
+	            }
+	          }
+	        ]
+	      }
+	    }
+	  }
+	}';
+	
+	return $ReturnValue;
+}
+?>
